069f154092162ddf36a98167a97020577e57759e
mofuel <miguel.monzon.lucio@gmail.com>
2024-10-22 12:53:59 -0500

    metodo para realizar una reserva

--- src/main/java/testing/Controlador/ControlReservas.java
+++ src/main/java/testing/Controlador/ControlReservas.java
@@ -1,13 +1,43 @@
 
 package testing.Controlador;
 
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import testing.Servicio.ReservaService;
+import testing.modelo.Reserva;
 
 @Controller
 public class ControlReservas {
+    
+    @Autowired
+    private ReservaService reservaservice;
+    
+    List<String> listaespacio = Arrays.asList("A1","A2","A3","A4","B1","B2","B3","B4");
+    
+    
     @GetMapping("/reservas")
-    public String mostrarReservas() {
-        return "Reservas"; // Nombre del archivo HTML
+    public String mostrarReservas(Model modelo) {
+        Reserva res = new Reserva();
+        modelo.addAttribute("reserva",res);
+        modelo.addAttribute("espacio", listaespacio);
+        return "Reservas"; 
     }  
+    
+    @GetMapping("/guarres")
+    public String GuadarRes(@ModelAttribute("reserva") Reserva res){
+        reservaservice.save(res);
+        return "index";
+    }
+    
+    
+    
+    
+    
+    
+    
 }
--- src/main/java/testing/Controlador/ReservaController.java
+++ src/main/java/testing/Controlador/ReservaController.java
@@ -2,6 +2,7 @@ package testing.Controlador;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import testing.modelo.Reserva;
@@ -9,10 +10,14 @@ import testing.modelo.Reserva;
 public class ReservaController {
     @PostMapping("/reserva")
     public String handleReserva(@ModelAttribute Reserva nuevaReserva, Model model) {
-        // Agrega la reserva al modelo
+        
         model.addAttribute("reserva", nuevaReserva);
         
-        // Redirige a la página de confirmación
-        return "reserva"; // Esto se traduce en /src/main/resources/templates/reserva.html
+        return "newreserva"; 
     }
+    
+    
+    
+   
+    
 }
